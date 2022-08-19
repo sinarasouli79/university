7391bf71a276451fa3acb64a2b3cc2b2ac237824
sina rasouli <sinarasouli79@gmail.com>
2022-08-19 18:24:12 +0430

    connect to the mysql

--- university/settings.py
+++ university/settings.py
@@ -77,10 +77,13 @@ WSGI_APPLICATION = 'university.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': 'mysql.cnf',
+        },
     }
 }
+
 
 
 # Password validation
