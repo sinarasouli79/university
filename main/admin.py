0853a534253288725ca99546620a21c79dfdb32d
sinarasouli79 <sinarasouli79@gmail.com>
2022-08-21 16:32:13 +0430

    congure admin and models to represent data farsi.

--- main/admin.py
+++ main/admin.py
@@ -6,15 +6,78 @@ from .models import (Faculty, Major, Student, Instructor, MajorInstructor,
 class FacultyAdmin(admin.ModelAdmin):
     list_display = ['name', 'manager', 'established_year']
     list_filter = ['established_year']
+    search_fields = ['name', 'manager', 'established_year']
+    ordering = ['-name']
+
+
+class MajorAdmin(admin.ModelAdmin):
+    list_display = ['name', 'area_of_interest', 'grade', 'faculty']
+    list_filter = ['name', 'area_of_interest']
+    search_fields = ['name', 'area_of_interest']
+    ordering = ['-name']
+
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'major']
+    list_filter = ['first_name', 'last_name', 'major']
+    search_fields = ['first_name', 'last_name']
+    ordering = ['-first_name', '-last_name', '-major']
+
+
+class MajorInstructorAdmin(admin.ModelAdmin):
+    list_display = ['major', 'instructor']
+    list_filter = ['major', 'instructor']
+    search_fields = ['instructor']
+    ordering = ['-instructor', '-major']
+
+
+class InstructorAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'get_majors', 'get_courses']
+    list_filter = ['first_name', 'last_name']
+    search_fields = ['first_name', 'last_name']  # , 'major', 'course']
+    ordering = ['-first_name', '-last_name']
+
+
+class InstructorCourseAdmin(admin.ModelAdmin):
+    list_display = ['course', 'instructor', ]
+    list_filter = ['instructor', 'course']
+    search_fields = ['instructor']
+    ordering = ['-course', '-instructor']
+
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['name', 'credit', 'get_majors', 'get_prerequisite']
+
+
+class MajorCourseAdmin(admin.ModelAdmin):
+    list_display = ['major', 'course']
+    list_filter = ['major', 'course']
+    search_fields = ['major', 'course']
+    ordering = ['-major', '-course']
+
+
+class SectionAdmin(admin.ModelAdmin):
+    list_display = ['course', 'instructor', 'class_time']
+    list_filter = ['class_time', 'course', 'instructor']
+    search_fields = ['class_time', 'course', 'instructor']
+    ordering = ['-course', '-instructor', '-class_time']
+
+
+class CourseSelectionAdmin(admin.ModelAdmin):
+    list_display = ['student', 'section']
+    list_filter = ['student', 'section']
+    search_fields = ['student', 'section']
+    ordering = ['-section', '-student']
+
 
 # Register your models here.
 admin.site.register(Faculty, FacultyAdmin)
-admin.site.register(Major)
-admin.site.register(Student)
-admin.site.register(Instructor)
-admin.site.register(MajorInstructor)
-admin.site.register(MajorCourse)
-admin.site.register(Course)
-admin.site.register(InstructorCourse)
-admin.site.register(Section)
-admin.site.register(CourseSelection)
+admin.site.register(Major, MajorAdmin)
+admin.site.register(Student, StudentAdmin)
+admin.site.register(Instructor, InstructorAdmin)
+admin.site.register(MajorInstructor, MajorInstructorAdmin)
+admin.site.register(InstructorCourse, InstructorCourseAdmin)
+admin.site.register(MajorCourse, MajorCourseAdmin)
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Section, SectionAdmin)
+admin.site.register(CourseSelection, CourseSelectionAdmin)
--- main/models.py
+++ main/models.py
@@ -4,9 +4,9 @@ from django.db import models
 
 
 class Faculty(models.Model):
-    name = models.CharField(max_length=30)
-    manager = models.CharField(max_length=30)
-    established_year = models.DateField()
+    name = models.CharField(max_length=30, verbose_name='نام')
+    manager = models.CharField(max_length=30, verbose_name='مدیر')
+    established_year = models.DateField(verbose_name='سال تاسیس')
 
     class Meta:
         verbose_name = 'دانشکده'
@@ -21,27 +21,31 @@ class Major(models.Model):
     MASTER = 'M'
     DOCTORATE = 'D'
     GRADE_CHOISES = [
-        (BACHELOR, 'BACHELOR'),
-        (MASTER, 'MASTER'),
-        (DOCTORATE, 'DOCTORATE'),
+        (BACHELOR, 'کارشناسی'),
+        (MASTER, 'کارشناسی ارشد'),
+        (DOCTORATE, 'دکترا'),
     ]
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, verbose_name='نام')
+    area_of_interest = models.CharField(
+        max_length=50, null=True, blank=True, verbose_name='گرایش')
     grade = models.CharField(
-        max_length=1, choices=GRADE_CHOISES, default=BACHELOR)
-    faculty = models.ForeignKey(Faculty, on_delete=models.PROTECT)
+        max_length=1, choices=GRADE_CHOISES, default=BACHELOR, verbose_name='مقطع')
+    faculty = models.ForeignKey(
+        Faculty, on_delete=models.PROTECT, verbose_name='دانشکده')
 
     class Meta:
         verbose_name = 'رشته'
         verbose_name_plural = 'رشته‌ها'
 
     def __str__(self) -> str:
-        return f'{self.name}'
+        return f'{self.name}-{self.area_of_interest}'
 
 
 class Student(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    major = models.ForeignKey(Major, on_delete=models.PROTECT)
+    first_name = models.CharField(max_length=30, verbose_name='نام')
+    last_name = models.CharField(max_length=30, verbose_name='نام‌خانوادگی')
+    major = models.ForeignKey(
+        Major, on_delete=models.PROTECT, verbose_name='رشته')
 
     class Meta:
         verbose_name = 'دانشجو'
@@ -52,8 +56,10 @@ class Student(models.Model):
 
 
 class MajorInstructor(models.Model):
-    major = models.ForeignKey(Major, on_delete=models.PROTECT)
-    instructor = models.ForeignKey('Instructor', on_delete=models.PROTECT)
+    major = models.ForeignKey(
+        Major, on_delete=models.PROTECT, verbose_name='رشته')
+    instructor = models.ForeignKey(
+        'Instructor', on_delete=models.PROTECT, verbose_name='مدرس')
 
     class Meta:
         verbose_name = 'مدرس های رشته'
@@ -64,10 +70,12 @@ class MajorInstructor(models.Model):
 
 
 class Instructor(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    major = models.ManyToManyField(Major, through=MajorInstructor)
-    course = models.ManyToManyField('Course', through='InstructorCourse')
+    first_name = models.CharField(max_length=30, verbose_name='نام')
+    last_name = models.CharField(max_length=30, verbose_name='نام‌خانوادگی')
+    majors = models.ManyToManyField(
+        Major, through=MajorInstructor, verbose_name='رشته')
+    courses = models.ManyToManyField(
+        'Course', through='InstructorCourse', verbose_name='درس')
 
     class Meta:
         verbose_name = 'مدرس'
@@ -76,10 +84,22 @@ class Instructor(models.Model):
     def __str__(self) -> str:
         return f'{self.first_name}-{self.last_name}'
 
+    def get_majors(self):
+        return '، '.join([f'{major.name} ({major.area_of_interest})' for major in self.majors.all()])
+
+    get_majors.short_description = 'رشته‌ها'
+
+    def get_courses(self):
+        return '، '.join([course.name for course in self.courses.all()])
+
+    get_courses.short_description = 'درس‌ها'
+
 
 class InstructorCourse(models.Model):
-    course = models.ForeignKey('Course', on_delete=models.PROTECT)
-    instructor = models.ForeignKey('Instructor', on_delete=models.PROTECT)
+    course = models.ForeignKey(
+        'Course', on_delete=models.PROTECT, verbose_name='درس')
+    instructor = models.ForeignKey(
+        'Instructor', on_delete=models.PROTECT, verbose_name='مدرس')
 
     class Meta:
         verbose_name = 'درس ارائه شده'
@@ -90,12 +110,13 @@ class InstructorCourse(models.Model):
 
 
 class Course(models.Model):
-    name = models.CharField(max_length=30)
-    credit = models.SmallIntegerField()
-    major = models.ManyToManyField('Major', through='MajorCourse')
+    name = models.CharField(max_length=30, verbose_name='نام')
+    credit = models.SmallIntegerField(verbose_name='تعداد واحد')
+    major = models.ManyToManyField(
+        'Major', through='MajorCourse', verbose_name='رشته')
 
     prerequisite = models.ManyToManyField(
-        'self', blank=True)
+        'self', blank=True, verbose_name='دروس پیشنیاز')
 
     class Meta:
         verbose_name = 'درس'
@@ -104,10 +125,20 @@ class Course(models.Model):
     def __str__(self) -> str:
         return f'{self.name}-{self.credit}'
 
+    def get_majors(self):
+        return '، '.join([major.name for major in self.major.all()])
+    get_majors.short_description = 'رشته‌ها'
+
+    def get_prerequisite(self):
+        return '، '.join([course.name for course in self.prerequisite.all()])
+    get_prerequisite.short_description = 'دروس پیشنیاز'
+
 
 class MajorCourse(models.Model):
-    major = models.ForeignKey(Major, on_delete=models.PROTECT)
-    course = models.ForeignKey('Course', on_delete=models.PROTECT)
+    major = models.ForeignKey(
+        Major, on_delete=models.PROTECT, verbose_name='رشته')
+    course = models.ForeignKey(
+        'Course', on_delete=models.PROTECT, verbose_name='درس')
 
     class Meta:
         verbose_name = 'درس های رشته'
@@ -118,10 +149,13 @@ class MajorCourse(models.Model):
 
 
 class Section(models.Model):
-    class_time = models.CharField(max_length=100)
-    course = models.ForeignKey(Course, on_delete=models.CASCADE)
-    instructor = models.ForeignKey(Instructor, on_delete=models.CASCADE)
-    student = models.ManyToManyField(Student, through='CourseSelection')
+    class_time = models.CharField(max_length=100, verbose_name='زمان')
+    course = models.ForeignKey(
+        Course, on_delete=models.CASCADE, verbose_name='درس')
+    instructor = models.ForeignKey(
+        Instructor, on_delete=models.CASCADE, verbose_name='مدرس')
+    student = models.ManyToManyField(
+        Student, through='CourseSelection', verbose_name='دانشجویان')
 
     class Meta:
         verbose_name = 'سکشن'
@@ -130,10 +164,16 @@ class Section(models.Model):
     def __str__(self) -> str:
         return f'{self.course}-{self.instructor}-{self.class_time}'
 
+    def get_students(self):
+        return '، '.join([student.id for student in self.student.all()])
+    get_students.short_description = 'دانشجویان'
+
 
 class CourseSelection(models.Model):
-    student = models.ForeignKey(Student, on_delete=models.CASCADE)
-    section = models.ForeignKey(Section, on_delete=models.CASCADE)
+    student = models.ForeignKey(
+        Student, on_delete=models.CASCADE, verbose_name='دانشجو')
+    section = models.ForeignKey(
+        Section, on_delete=models.CASCADE, verbose_name='سکشن')
 
     class Meta:
         verbose_name = 'انتخاب واحد'
