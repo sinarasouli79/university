d40bf95708cf4113fe2bc581ee89e9742beebba2
sina rasouli <sinarasouli79@gmail.com>
2022-08-21 06:17:51 +0430

    initial commit

--- main/admin.py
+++ main/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from .models import (Faculty, Major, Student, Instructor, MajorInstructor,
                      MajorCourse, Course, InstructorCourse, Section, CourseSelection)
+
+
+class FacultyAdmin(admin.ModelAdmin):
+    list_display = ['name', 'manager', 'established_year']
+    list_filter = ['established_year']
+
 # Register your models here.
-admin.site.register(Faculty)
+admin.site.register(Faculty, FacultyAdmin)
 admin.site.register(Major)
 admin.site.register(Student)
 admin.site.register(Instructor)
