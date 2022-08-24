575034c4bf006d0ed298e268d8b73844db603f6d
sinarasouli79 <sinarasouli79@gmail.com>
2022-08-24 19:10:44 +0430

    feat:add list view for all models class
    add endpoint for faculty, instructor, student and course models.

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import student_list
+from .views import FacultyList, InstructorList, StudentList, CourseList
 app_name = 'main'
 
 urlpatterns = [
-    path('', student_list, name='student-list')
+    path('faculty/', FacultyList.as_view(), name='faculty-list'),
+    path('instructor/', InstructorList.as_view(), name='instructor-list'),
+    path('student/', StudentList.as_view(), name='student-list'),
+    path('course/', CourseList.as_view(), name='course-list'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,9 +1,49 @@
-from django.shortcuts import render
-from .models import (Student)
+from django.views.generic import ListView
+from .models import (Faculty, Instructor, Student, Course, Major,
+                     MajorInstructor, InstructorCourse, MajorCourse, Section, CourseSelection)
 # Create your views here.
 
 
-def student_list(request, *arg, **kwargs):
+class FacultyList(ListView):
+    queryset = Faculty.objects.all()
+    template_name = 'list_view.html'
+
+
+class InstructorList(ListView):
+    queryset = Instructor.objects.all()
+    template_name = 'list_view.html'
+
+
+class StudentList(ListView):
     queryset = Student.objects.all()
-    context = {'queryset': queryset}
-    return render(request, 'student-detail-list.html', context)
+    template_name = 'list_view.html'
+
+
+class CourseList(ListView):
+    queryset = Course.objects.all()
+    template_name = 'list_view.html'
+
+
+class MajorInstructorList(ListView):
+    queryset = MajorInstructor.objects.all()
+    template_name = 'list_view.html'
+
+
+class InstructorCourseList(ListView):
+    queryset = InstructorCourse.objects.all()
+    template_name = 'list_view.html'
+
+
+class MajorCourseList(ListView):
+    queryset = MajorCourse.objects.all()
+    template_name = 'list_view.html'
+
+
+class SectionList(ListView):
+    queryset = Section.objects.all()
+    template_name = 'list_view.html'
+
+
+class CourseSelectionList(ListView):
+    queryset = CourseSelection.objects.all()
+    template_name = 'list_view.html'
